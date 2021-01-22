876179f741c7e25a2015899022fdb7a8157bc258
unknown <xreider@yandex.ru>
2021-01-22 12:58:52 +1000

    e3e

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+express.json()
+express.urlencoded({extended: true})
+
+// const options = {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     credentials: true
+//   }
+// };
+// const io = require('socket.io')(server, options)
+
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+
+
+  console.log('IO connected')
+  socket.on('destroy', () => {
+    console.log('destroyed')
+  })
+  socket.on('hello', () => {
+    console.log('hello')
+  })
+})
+
+module.exports = {
+  app, server
+}
